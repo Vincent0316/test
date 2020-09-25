ea18e9c3a988ea9dbae71a071345663881ba9e93
nameVal <email@qq.com>
2020-09-25 13:03:02 +0800

    csac

--- routes/index.js
+++ routes/index.js
@@ -2,63 +2,49 @@ var express = require("express");
 var router = express.Router();
 
 var passport = require("passport");
-// var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 var userModel = require("./../db/db");
 
-// passport.use(userModel.createStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "425773347497-athni9dhsp3259tllu4as507gkhp89a0.apps.googleusercontent.com",
+      clientSecret: "9KwAOXMZgUprUH6gzNnoPMia",
+      //   callbackURL: "http://127.0.0.1:3000/index/googleCallback",
+      callbackURL:
+        "https://young-refuge-40202.herokuapp.com/index/googleCallback",
+      proxy: true,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log("done -> :", done);
+      console.log("profile -> :", profile);
+      console.log("refreshToken -> :", refreshToken);
+      console.log("accessToken -> :", accessToken);
+      //   在这里保存数据
+      var us = new userModel({
+        country: '',
+        firstName: profile.name.familyName,
+        lastName: profile.name.givenName,
+        username: profile.name.familyName + '@qq.com',
+        email: profile.name.familyName + '@qq.com',
+        password: '88888888',
+        repassword: '',
+        address: '',
+        city: '',
+        state: '',
+        code: '',
+        phone: '',
+      })
+      // us.save()
+      userModel.register(us, '88888888', (err, user) => {
+        console.log("保存的 user -> :", user);
 
-// used to serialize the user for the session
-// passport.serializeUser(function (user, done) {
-//   done(null, user.id);
-// });
-// used to deserialize the user
-// passport.deserializeUser(function (id, done) {
-//   done(id);
-// User.findById(id, function (err, user) {
-//   done(err, user);
-// });
-// });
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID:
-//         "425773347497-athni9dhsp3259tllu4as507gkhp89a0.apps.googleusercontent.com",
-//       clientSecret: "9KwAOXMZgUprUH6gzNnoPMia",
-//       //   callbackURL: "http://127.0.0.1:3000/index/googleCallback",
-//       callbackURL:
-//         "https://young-refuge-40202.herokuapp.com/index/googleCallback",
-//       proxy: true,
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//       console.log("done -> :", done);
-//       console.log("profile -> :", profile);
-//       console.log("refreshToken -> :", refreshToken);
-//       console.log("accessToken -> :", accessToken);
-//       //   在这里保存数据
-//       var us = new userModel({
-//         country: '',
-//         firstName: profile.name.familyName,
-//         lastName: profile.name.givenName,
-//         username: profile.name.familyName + '@qq.com',
-//         email: profile.name.familyName + '@qq.com',
-//         password: '88888888',
-//         repassword: '',
-//         address: '',
-//         city: '',
-//         state: '',
-//         code: '',
-//         phone: '',
-//       })
-//       // us.save()
-//       userModel.register(us, '88888888', (err, user) => {
-//         console.log("保存的 user -> :", user);
-
-//         done()
-//       });
-//     }
-//   )
-// );
+        done()
+      });
+    }
+  )
+);
 
 router.get(
   "/alluser",
@@ -83,8 +69,8 @@ router.get(
 router.get(
   "/googleLogin",
   passport.authenticate("google", {
-    // scope: ["https://www.googleapis.com/auth/plus.login"],
-    scope: ['profile', 'email']
+    scope: ["https://www.googleapis.com/auth/plus.login"],
+    // scope: ['profile', 'email']
   })
 );
 
--- routes/users.js
+++ routes/users.js
@@ -3,53 +3,8 @@ var userModel = require("./../db/db");
 var bcryptjs = require("bcryptjs");
 var router = express.Router();
 const passport = require("passport");
-var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-
 
 passport.use(userModel.createStrategy());
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        "425773347497-athni9dhsp3259tllu4as507gkhp89a0.apps.googleusercontent.com",
-      clientSecret: "9KwAOXMZgUprUH6gzNnoPMia",
-      //   callbackURL: "http://127.0.0.1:3000/index/googleCallback",
-      callbackURL:
-        "https://young-refuge-40202.herokuapp.com/index/googleCallback",
-      proxy: true,
-    },
-    function (accessToken, refreshToken, profile, done) {
-      console.log("done -> :", done);
-      console.log("profile -> :", profile);
-      console.log("refreshToken -> :", refreshToken);
-      console.log("accessToken -> :", accessToken);
-      //   在这里保存数据
-      var us = new userModel({
-        country: '',
-        firstName: profile.name.familyName,
-        lastName: profile.name.givenName,
-        username: profile.name.familyName + '@qq.com',
-        email: profile.name.familyName + '@qq.com',
-        password: '88888888',
-        repassword: '',
-        address: '',
-        city: '',
-        state: '',
-        code: '',
-        phone: '',
-      })
-      // us.save()
-      userModel.register(us, '88888888', (err, user) => {
-        console.log("保存的 user -> :", user);
-
-        done()
-      });
-    }
-  )
-);
-
-
 passport.serializeUser(userModel.serializeUser());
 passport.deserializeUser(userModel.deserializeUser());
 
