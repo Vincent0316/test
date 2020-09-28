6516638d4c59a1a5c8ac277a2e2533ffbd6ed206
Qiyao Lyu <lyuq2@cardiff.ac.uk>
2020-09-28 10:29:01 -0400

    a

--- app.js
+++ app.js
@@ -73,10 +73,6 @@ app.post('/google', function(req, res) {
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
     });
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-    // If request specified a G Suite domain:
-    // const domain = payload['hd'];
   }
   verify()
   .then(()=>{
